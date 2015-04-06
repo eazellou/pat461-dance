b6fb5d5fcf2c1fe2ca3074c24528331eb42b1c4f
eazellou <emmaazelborn@gmail.com>
2015-04-06 15:06:44 -0400

    CMap for more interesting timbre, non-linearity also mapped to acceleration

--- modulation.lua
+++ modulation.lua
@@ -3,7 +3,7 @@ FreeAllRegions()
 DPrint("")
 dac= FBDac
 
-sinosc = FlowBox(FBSinOsc)
+sinosc = FlowBox(FBCMap)
 
 asymp = FlowBox(FBAsymp)
 energyPush = FlowBox(FBPush)
@@ -14,63 +14,60 @@ sinosc.Amp:SetPull(asymp.Out)
 freqPush = FlowBox(FBPush)
 freqSymp = FlowBox(FBAsymp)
 freqPush.Out:SetPush(freqSymp.In)
-freqSymp.In:SetPull(freqPush.Out)
 sinosc.Freq:SetPull(freqSymp.Out)
 
---linPush = FlowBox(FBPush)
---linPush.Out:SetPush(sinosc.NonL)
---sinosc.NonL:SetPull(linPush.Out)
+linPush = FlowBox(FBPush)
+linSymp = FlowBox(FBAsymp)
+linSymp.In:SetPull(linPush.Out)
+sinosc.NonL:SetPull(linSymp.Out)
 
-sinosc2 = FlowBox(FBSinOsc)
+sinosc2 = FlowBox(FBCMap)
 asymp2 = FlowBox(FBAsymp)
 energyPush2 = FlowBox(FBPush)
 energyPush2.Out:SetPush(asymp2.In)
-asymp2.In:SetPull(energyPush2.Out)
 sinosc2.Amp:SetPull(asymp2.Out)
 
 freqPush2 = FlowBox(FBPush)
 freqSymp2 = FlowBox(FBAsymp)
 freqPush2.Out:SetPush(freqSymp2.In)
-freqSymp2.In:SetPull(freqPush2.Out)
 sinosc2.Freq:SetPull(freqSymp2.Out)
 
---linPush2 = FlowBox(FBPush)
---linPush2.Out:SetPush(sinosc2.NonL)
---sinosc2.NonL:SetPull(linPush2.Out)
+linPush2 = FlowBox(FBPush)
+linSymp2 = FlowBox(FBAsymp)
+linSymp2.In:SetPull(linPush2.Out)
+sinosc2.NonL:SetPull(linSymp2.Out)
 
-sinosc3 = FlowBox(FBSinOsc)
+sinosc3 = FlowBox(FBCMap)
 asymp3 = FlowBox(FBAsymp)
 energyPush3 = FlowBox(FBPush)
 energyPush3.Out:SetPush(asymp3.In)
-asymp3.In:SetPull(energyPush3.Out)
 sinosc3.Amp:SetPull(asymp3.Out)
 
 freqPush3 = FlowBox(FBPush)
 freqSymp3 = FlowBox(FBAsymp)
 freqPush3.Out:SetPush(freqSymp3.In)
-freqSymp3.In:SetPull(freqPush3.Out)
 sinosc3.Freq:SetPull(freqSymp3.Out)
 
---linPush3 = FlowBox(FBPush)
---linPush3.Out:SetPush(sinosc3.NonL)
---sinosc3.NonL:SetPull(linPush3.Out)
+linPush3 = FlowBox(FBPush)
+linSymp3 = FlowBox(FBAsymp)
+linSymp3.In:SetPull(linPush3.Out)
+sinosc3.NonL:SetPull(linSymp3.Out)
 
-sinosc4 = FlowBox(FBSinOsc)
+sinosc4 = FlowBox(FBCMap)
 asymp4 = FlowBox(FBAsymp)
 energyPush4 = FlowBox(FBPush)
 energyPush4.Out:SetPush(asymp4.In)
-asymp4.In:SetPull(energyPush4.Out)
 sinosc4.Amp:SetPull(asymp4.Out)
 
 freqPush4 = FlowBox(FBPush)
 freqSymp4 = FlowBox(FBAsymp)
 freqPush4.Out:SetPush(freqSymp4.In)
-freqSymp4.In:SetPull(freqPush4.Out)
 sinosc4.Freq:SetPull(freqSymp4.Out)
 
---linPush4 = FlowBox(FBPush)
---linPush4.Out:SetPush(sinosc4.NonL)
---sinosc4.NonL:SetPull(linPush4.Out)
+linPush4 = FlowBox(FBPush)
+linSymp4 = FlowBox(FBAsymp)
+linSymp4.In:SetPull(linPush4.Out)
+sinosc4.NonL:SetPull(linSymp4.Out)
 
 
 dac.In:SetPull(sinosc.Out)
@@ -80,10 +77,10 @@ dac.In:SetPull(sinosc4.Out)
 
 freqPush:Push(.5)
 energyPush:Push(.5)
---linPush:Push(-1)
---linPush2:Push(-1)
---linPush3:Push(-1)
---linPush4:Push(-1)
+linPush:Push(0)
+linPush2:Push(0)
+linPush3:Push(0)
+linPush4:Push(0)
 
 
 function round(num, idp)
@@ -98,8 +95,14 @@ end
 
 function addEnergy(energy)
 	DPrint(energy)
-	nonLin1 = energy/2 - 1
-	--linPush:Push(nonLin1)
+	nonLin1 = energy
+	nonLin2 = 3*energy/4
+	nonLin3 = energy/2
+	nonLin4 = energy/4
+	linPush:Push(nonLin1)
+	linPush2:Push(nonLin2)
+	linPush3:Push(nonLin3)
+	linPush4:Push(nonLin4)
 	energy1 = energy
 	energy2 = energy/2
 	energy3 = energy/4
@@ -137,11 +140,11 @@ function accel(self, x, y, z)
 	--DPrint(magnitude)
 	--magnitude ranges about .98 - 3.5
 	energy = (magnitude - 1)*.7
-	if energy > 1 then
-		energy = 1
+	if energy > .8 then
+		energy = .8
 	end
-	if energy < 0 then
-		energy = 0
+	if energy < .05 then
+		energy = .05
 	end
 	--energy ranges about .28 - 1
 	--DPrint(accX)
