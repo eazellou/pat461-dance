a4ff7a85f8b1f0b757fc5300e6620100fa3ee180
quzhi65222714 <quzhi65222714@gmail.com>
2015-04-08 17:01:18 -0400

    Bug fixed, working~~~~

--- slave.lua
+++ slave.lua
@@ -12,12 +12,19 @@ function setRotate(self, x, y, z)
 	zr = z
 end
 
-host = "35.2.179.193"
+host = "35.2.144.72"
 port = 8888
 
-function sendAccel(self, x, y, z)
-	DPrint("Sending\nRotation: "..xr.." "..yr.." "..zr.." ".."\nAccel: "..x.." "..y.." "..z)
-	SendOSCMessage(host, port, "/urMus/numbers", xr, yr, zr, xa, ya, za)
+function sendAccel(self, xa, ya, za)
+	DPrint("Sending\nRotation: "..xr.." "..yr.." "..zr.." ".."\nAccel: "..xa.." "..ya.." "..za)
+	-- SendOSCMessage(host, portR, "/urMus/numbers", xr, yr, zr)
+	-- SendOSCMessage(host, portA, "/urMus/numbers", xa, ya, za)
+	SendOSCMessage(host, port, "/urMus/numbers", 1, xr)
+	SendOSCMessage(host, port, "/urMus/numbers", 2, yr)
+	SendOSCMessage(host, port, "/urMus/numbers", 3, zr)
+	SendOSCMessage(host, port, "/urMus/numbers", 4, xa)
+	SendOSCMessage(host, port, "/urMus/numbers", 5, ya)
+	SendOSCMessage(host, port, "/urMus/numbers", 6, za)
 end
 
 r = Region()
